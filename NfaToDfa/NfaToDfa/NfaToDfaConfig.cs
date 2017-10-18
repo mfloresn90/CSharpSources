8d170fdd56ec42aa6ceaeaad3e77c8ca94dc081e
Mauricio Flores <yoshiblack90@gmail.com>
2017-10-17 23:34:05 -0500

    Convert No-Deterministic Finite Automata to Deterministic Finite Automata

---
+++ NfaToDfa/NfaToDfa/NfaToDfaConfig.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace NfaToDfa
+{
+    public class NfaToDfaConfig
+    {
+
+        String absolutePath;
+        int cardQ;
+        int cardF;
+        int[] f;
+        int[] cardSn;
+        int[,] positionDeltaN;
+        List<int[]> deltaN;
+        List<int[]> transitionT;
+        List<int> preLclosure = new List<int>();
+
+        public NfaToDfaConfig() { }
+
+        public NfaToDfaConfig(String absolutePath)
+        {
+            this.absolutePath = absolutePath;
+            readNfaData();
+        }
+
+        public int getCardQ()
+        {
+            return cardQ;
+        }
+
+        public int getCardF()
+        {
+            return cardF;
+        }
+
+        public int[] getF()
+        {
+            return f;
+        }
+
+        public List<int[]> getDeltaN()
+        {
+            return deltaN;
+        }
+
+        public int[] getCardSn()
+        {
+            return cardSn;
+        }
+
+        public int[,] getPositionDeltaN()
+        {
+            return positionDeltaN;
+        }
+
+        public List<int[]> getTransitionT()
+        {
+            return transitionT;
+        }
+
+        public void currentPath()
+        {
+            string startupPath = Directory.GetCurrentDirectory();
+            Console.WriteLine("CurrentPath: " + startupPath);
+        }
+
+        public void readNfaData()
+        {
+            int counter = 0;
+            int[] tmp;
+            string[] separators = { ",", " " };
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(absolutePath))
+                {
+
+                    for (String line; (line = sr.ReadLine()) != null;)
+                    {
+                        if (counter == 0)
+                        {
+                            cardQ = int.Parse(line);
+                        }
+                        else if (counter == 1)
+                        {
+                            cardF = int.Parse(line);
+                        }
+                        else if (counter == 2)
+                        {
+                            String[] infoF = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                            int fValue = 0;
+                            f = new int[infoF.Length];
+                            for (int i = 0; i < infoF.Length; i++)
+                            {
+                                Int32.TryParse(infoF[i], out fValue);
+                                f[i] = fValue;
+                            }
+                        }
+                        else if (counter == 3)
+                        {
+                            String[] infoSa = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                            cardSn = new int[infoSa.Length];
+                            int SaValue = 0;
+                            for (int i = 0; i < infoSa.Length; i++)
+                            {
+                                Int32.TryParse(infoSa[i], out SaValue);
+                                cardSn[i] = SaValue;
+                            }
+                            positionDeltaN = new int[cardSn[0], cardSn[1]];
+                            int position = 0;
+                            for (int x = 0; x < cardSn[0]; x++)
+                            {
+                                for (int y = 0; y < cardSn[1]; y++)
+                                {
+                                    positionDeltaN[x, y] = position;
+                                    position++;
+                                }
+                            }
+                        }
+                        else if (counter == 4)
+                        {
+                            deltaN = new List<int[]>();
+                            transitionT = new List<int[]>();
+                            String[] sa = line.Split('|');
+                            for (int i = 0; i < sa.Length; i++)
+                            {
+                                String con = sa[i];
+                                String[] sap = con.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                                tmp = new int[sap.Length];
+                                int sapValue = 0;
+                                for (int j = 0; j < sap.Length; j++)
+                                {
+                                    Int32.TryParse(sap[j], out sapValue);
+                                    tmp[j] = sapValue;
+                                }
+                                deltaN.Add(tmp);
+                            }
+                            for (int i = 0; i < sa.Length; i += 3)
+                            {
+                                String con = sa[i + 2];
+                                String[] sap = con.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                                tmp = new int[sap.Length];
+                                int sapValue = 0;
+                                for (int j = 0; j < sap.Length; j++)
+                                {
+                                    Int32.TryParse(sap[j], out sapValue);
+                                    tmp[j] = sapValue;
+                                }
+                                transitionT.Add(tmp);
+                            }
+                        }
+                        counter++;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("readNfaData.IOException Message: " + e.Message);
+            }
+        }
+
+        public List<int[]> getLambdaClosure(int cardQ)
+        {
+            List<int[]> result = new List<int[]>();
+            Stack<int> lifo = new Stack<int>();
+            int[] getInfo;
+            for (int i = cardQ - 1; i >= 0; i--)
+            {
+                lifo.Push(i);
+            }
+            while (lifo.Count > 0)
+            {
+                preLclosure.Clear();
+                int lifoValue = lifo.Pop();
+                getPreLambdaClosure(lifoValue);
+                getInfo = new int[preLclosure.Count];
+                for (int i = 0; i < preLclosure.Count; i++)
+                {
+                    getInfo[i] = preLclosure[i];
+                }
+                result.Add(getInfo);
+            }
+            return result;
+        }
+
+        void getPreLambdaClosure(int state)
+        {
+            preLclosure.Add(state);
+            int[] finded = transitionT[state];
+            for (int i = 0; i < finded.Length; i++)
+            {
+                if (finded[i] != state)
+                {
+                    getPreLambdaClosure(finded[i]);
+                }
+            }
+        }
+
+        public int[] move(int[] s, int c)
+        {
+            List<int> preResult = new List<int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != deltaN[positionDeltaN[s[i], c]][0])
+                {
+                    preResult.Add(deltaN[positionDeltaN[s[i], c]][0]);
+                }
+
+            }
+            int[] result = new int[preResult.Count];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = preResult[i];
+            }
+            return result;
+        }
+
+        public int[] lClosure(int[] move, List<int[]> lambdaClosure)
+        {
+            List<int> preResult = new List<int>();
+            int[] preRes;
+            int[] result;
+            for (int i = 0; i < move.Length; i++)
+            {
+                preRes = lambdaClosure[move[i]];
+                for (int j = 0; j < preRes.Length; j++)
+                {
+                    preResult.Add(preRes[j]);
+                }
+            }
+            preResult.Sort();
+            result = new int[preResult.Count];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = preResult[i];
+            }
+            return result;
+        }
+
+		public bool ArraysEqual<T>(T[] a1, T[] a2)
+		{
+			if (ReferenceEquals(a1, a2))
+				return true;
+
+			if (a1 == null || a2 == null)
+				return false;
+
+			if (a1.Length != a2.Length)
+				return false;
+
+			EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+			for (int i = 0; i < a1.Length; i++)
+			{
+				if (!comparer.Equals(a1[i], a2[i])) return false;
+			}
+			return true;
+		}
+
+        public bool uInDestados(List<int[]> dtran, int[] conjU)
+        {
+            bool flag = false;
+            foreach (int[] items in dtran)
+            {
+                if (!items.Equals(conjU))
+                {
+                    flag = true;
+                    //Console.WriteLine(" dtran --> " + "[" + string.Join(", ", items) + "]");
+                    //Console.WriteLine(" conjU --> " + "[" + string.Join(", ", conjU) + "]");
+                }
+            }
+            return flag;
+        }
+
+    }
+
+}
---
+++ NfaToDfa/NfaToDfa/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NfaToDfa
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            NfaToDfaConfig nfadfa = new NfaToDfaConfig("nfa.txt");
+            List<int[]> dTran = new List<int[]>();
+
+            Console.WriteLine("\nConversión de un AFN a AFD (AFN-a-AFD v1.0)\n");
+            Console.WriteLine("cardQ = " + nfadfa.getCardQ());
+            Console.WriteLine("cardF = " + nfadfa.getCardF());
+            Console.WriteLine("F = " + "[" + string.Join(", ", nfadfa.getF()) + "]");
+            Console.WriteLine("DeltaN = {");
+            for (int i = 0; i < nfadfa.getDeltaN().Count; i += 3)
+            {
+                Console.Write("\t");
+                Console.Write("[" + string.Join(", ", nfadfa.getDeltaN()[i]) + "]\t");
+                Console.Write("[" + string.Join(", ", nfadfa.getDeltaN()[i + 1]) + "]\t");
+                Console.WriteLine("[" + string.Join(", ", nfadfa.getDeltaN()[i + 2]) + "]");
+            }
+            Console.WriteLine("}");
+            List<int[]> lambdaClosure = nfadfa.getLambdaClosure(nfadfa.getCardQ());
+            Console.WriteLine("LambdaCerradura = {");
+            foreach (int[] getData in lambdaClosure)
+            {
+                Console.Write("\t");
+                Console.WriteLine("[" + string.Join(", ", getData) + "]");
+            }
+            Console.WriteLine("}\n");
+
+            dTran.Add(lambdaClosure[0]);
+            //Console.WriteLine("Estado inicial  (q0) --> Lambda --> " + "[" + string.Join(", ", dTran[0]) + "]");
+
+            int[] language = { 0, 1 };
+            int[] conjuntoU = null;
+            for (int i = 0; i < dTran.Count; i++)
+            {
+                for (int j = 0; j < language.Length; j++)
+                {
+                    int[] move = nfadfa.move(dTran[i], language[j]);
+                    //Console.Write(" --> Move with [" + language[j] + "] --> " + "[" + string.Join(", ", move) + "]");
+                    conjuntoU = nfadfa.lClosure(move, lambdaClosure);
+                    //Console.WriteLine(" --> Lambda --> " + "[" + string.Join(", ", conjuntoU) + "]");
+
+                    Console.WriteLine("\nNFA CURRENT STATES");
+                    foreach (int[] items in dTran)
+                    {
+                        Console.WriteLine("[" + string.Join(", ", items) + "]");
+                    }
+                    Console.WriteLine("\n");
+
+                    Console.Write("Agregar estado: [" + string.Join(", ", conjuntoU) + "]? (y/n): ");
+
+                    switch (Console.Read())
+                    {
+                        case 'y':
+                            Console.Write("\nAgregando a estados actuales");
+                            dTran.Add(conjuntoU);
+                            break;
+                        case 'n':
+                            Console.WriteLine("\nOmitiendo conjunto...");
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nFINAL STATES");
+            foreach (int[] items in dTran)
+            {
+                Console.WriteLine("[" + string.Join(", ", items) + "]");
+            }
+            Console.WriteLine("\n");
+
+            int index = 0;
+            List<int> dfaTran = new List<int>();
+            for (int i = 0; i < dTran.Count; i++)
+            {
+                for (int j = 0; j < language.Length; j++)
+                {
+                    int[] move = nfadfa.move(dTran[i], language[j]);
+                    //Console.Write(" --> Move with [" + language[j] + "] --> " + "[" + string.Join(", ", move) + "]");
+                    conjuntoU = nfadfa.lClosure(move, lambdaClosure);
+                    //Console.WriteLine(" --> Lambda --> " + "[" + string.Join(", ", conjuntoU) + "]");
+
+                    for (int k = 0; k < dTran.Count; k++)
+                    {
+                        if (nfadfa.ArraysEqual<int>(conjuntoU, dTran[k]))
+                        {
+                            index = k;
+                            dfaTran.Add(k);
+                        }
+                    }
+
+                    Console.Write("[" + j + "]: ");
+                    Console.Write("[" + string.Join(", ", conjuntoU) + "] = " + index);
+                    Console.Write("\t");
+                }
+                Console.WriteLine("\n");
+            }
+
+            int cardQ = dTran.Count;
+            Console.WriteLine("Ingresa cardF: ");
+            int cardF = Convert.ToInt32(Console.ReadLine());
+            int[] statesF = new int[cardF];
+            for (int i = 0; i < cardF; i++)
+            {
+                Console.Write("F[" + i + "]: ");
+                statesF[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int[] cardStates = { cardQ, 2 };
+
+
+
+            using (StreamWriter outputFile = new StreamWriter("dfa.txt"))
+            {
+                outputFile.WriteLine(cardQ);
+                outputFile.WriteLine(cardF);
+                outputFile.WriteLine(string.Join(", ", statesF));
+                outputFile.WriteLine(string.Join(", ", cardStates));
+                for (int i = 0; i < dfaTran.Count; i++)
+                {
+                    if (i < dfaTran.Count - 1)
+                    {
+                        outputFile.Write(dfaTran[i] + ", ");
+                    }
+                    else if (i == dfaTran.Count - 1)
+                    {
+                        outputFile.WriteLine(dfaTran[i]);
+                    }
+                }
+                outputFile.WriteLine("No disponible...");
+            }
+
+            Console.WriteLine("Archivo generado...");
+
+        }
+    }
+}
